c2f330080ea3c6174565c89413fe83ca5addf4e2
Anton Ovechkin <owanturist@gmail.com>
2021-10-12 21:46:50 +0200

    Configure elm-review

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -1,19 +1,58 @@
 module ReviewConfig exposing (config)
 
-{-| Do not rename the ReviewConfig module or the config function, because
-`elm-review` will look for these.
-
-To add packages that contain rules, add them to this review project using
-
-    `elm install author/packagename`
-
-when inside the directory containing this file.
-
--}
-
+import NoUnused.CustomTypeConstructorArgs
+import NoUnused.CustomTypeConstructors
+import NoUnused.Dependencies
+import NoUnused.Exports
+import NoUnused.Modules
+import NoUnused.Parameters
+import NoUnused.Patterns
+import NoUnused.Variables
+import NoUnoptimizedRecursion
+import Simplify
+import NoExposingEverything
+import NoImportingEverything
+import NoMissingTypeAnnotation
+import NoMissingTypeExpose
+import NoPrematureLetComputation
+import NoMissingSubscriptionsCall
+import NoUselessSubscriptions
+import NoDebug.Log
+import NoDebug.TodoOrToString
 import Review.Rule exposing (Rule)
 
 
 config : List Rule
 config =
-    []
+    -- github.com/jfmengels/elm-review-unused
+    [ NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.CustomTypeConstructorArgs.rule
+    , NoUnused.Dependencies.rule
+    , NoUnused.Exports.rule
+    , NoUnused.Modules.rule
+    , NoUnused.Parameters.rule
+    , NoUnused.Patterns.rule
+    , NoUnused.Variables.rule
+
+    -- github.com/jfmengels/elm-review-performance
+    , NoUnoptimizedRecursion.rule (NoUnoptimizedRecursion.optOutWithComment "IGNORE TCO")
+
+    -- github.com/jfmengels/elm-review-simplify
+    , Simplify.rule Simplify.defaults
+
+    -- github.com/jfmengels/elm-review-common
+    , NoExposingEverything.rule
+    , NoImportingEverything.rule []
+    , NoMissingTypeAnnotation.rule
+    , NoMissingTypeExpose.rule
+    , NoPrematureLetComputation.rule
+
+    -- github.com/jfmengels/elm-review-the-elm-architecture
+    , NoMissingSubscriptionsCall.rule
+    -- , NoRecursiveUpdate.rule
+    , NoUselessSubscriptions.rule
+
+    -- github.com/jfmengels/elm-review-debug
+    , NoDebug.Log.rule
+    , NoDebug.TodoOrToString.rule
+    ]
