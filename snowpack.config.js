0e69bf30cafdbde0638b68e102f54d6451764353
Anton Ovechkin <owanturist@gmail.com>
2021-10-14 21:24:55 +0200

    Change webpack to snowpack (#17)

---
+++ snowpack.config.js
@@ -0,0 +1,32 @@
+const fg = require('fast-glob')
+
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/dist'
+  },
+  plugins: [
+    ['snowpack-plugin-elm', { optimize: 'build' }],
+    '@jadex/snowpack-plugin-tailwindcss-jit' // fix for https://github.com/tailwindlabs/tailwindcss/issues/3950
+  ],
+  exclude: ['**/node_modules/**/*', 'review/**/*.elm'],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    {
+      match: 'all',
+      src: '(?!.*(.svg|.gif|.json|.jpg|.jpeg|.png|.js)).*',
+      dest: '/index.html'
+    }
+  ],
+  env: {
+    NODE_ENV: process.env.NODE_ENV
+  },
+  devOptions: {
+    open: 'none',
+    port: 3000
+  },
+  optimize: {
+    bundle: true,
+    minify: true
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { Elm } from './Main.elm'
+
+const { ports } = Elm.Main.init()
+
+if (ports.local_storage__get_item && ports.local_storage__get_item_done) {
+  ports.local_storage__get_item.subscribe(function (key) {
+    ports.local_storage__get_item_done.send([
+      key,
+      window.localStorage.getItem(key)
+    ])
+  })
+}
+
+if (ports.local_storage__set_item) {
+  ports.local_storage__set_item.subscribe(function (payload) {
+    window.localStorage.setItem(payload[0], payload[1])
+  })
+}
