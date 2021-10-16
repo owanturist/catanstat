b82e020bee1c2500cf5f4f3634ae82d2fb105a4d
Anton Ovechkin <owanturist@gmail.com>
2021-10-16 12:26:26 +0200

    Init Icon module

--- stories/Stories.elm
+++ stories/Stories.elm
@@ -1,9 +1,7 @@
 port module Stories exposing (main)
 
 import Bulletproof
-import Bulletproof.Knob
-import Html
-import Html.Attributes
+import Stories.Icon
 
 
 port save_settings : String -> Cmd msg
@@ -13,14 +11,5 @@ main : Bulletproof.Program
 main =
     Bulletproof.program
         save_settings
-        [ Bulletproof.folder "Example"
-            [ Bulletproof.story "with single knob"
-                (\txt ->
-                    Html.div
-                        [ Html.Attributes.class "w-[200px] bg-gray-200 p-4" ]
-                        [ Html.text txt ]
-                        |> Bulletproof.fromHtml
-                )
-                |> Bulletproof.Knob.string "Text" ":)"
-            ]
+        [ Stories.Icon.story
         ]
