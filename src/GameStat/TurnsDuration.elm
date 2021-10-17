b06ad29a395cab6273a49f7e316984adcc7c124b
Anton Ovechkin <owanturist@gmail.com>
2021-10-17 11:03:11 +0200

    Finaly make bars duration chart

--- src/GameStat/TurnsDuration.elm
+++ src/GameStat/TurnsDuration.elm
@@ -2,8 +2,10 @@ module GameStat.TurnsDuration exposing (Model, view)
 
 import Chart
 import Chart.Attributes
+import Chart.Svg
 import Html exposing (Html)
 import Html.Attributes
+import Html.Lazy
 import Player exposing (Player)
 
 
@@ -21,32 +23,30 @@ type alias Model =
 -- V I E W
 
 
-type alias Round =
-    List Int
+type alias Turn =
+    { index : Int
+    , duration : Int
+    , player : Player
+    }
 
 
-getTurnDurationFor : Int -> Round -> Maybe Int
-getTurnDurationFor index round =
-    List.head (List.drop index round)
-
-
-buildRounds : List Int -> List Player -> List Round
-buildRounds turns players =
+buildTurnsDurationData : List Int -> List Player -> List Turn
+buildTurnsDurationData turns players =
     let
-        totalPlayersCount =
-            List.length players
+        totalTurnsCount =
+            List.length turns
 
-        buildRoundsInner : List Int -> List Round -> List Round
-        buildRoundsInner remainingTurns acc =
-            if List.isEmpty remainingTurns then
-                acc
+        totalRoundsCount =
+            -- that's fine if it is longer than turns
+            1 + totalTurnsCount // List.length players
 
-            else
-                buildRoundsInner
-                    (List.drop totalPlayersCount remainingTurns)
-                    (List.take totalPlayersCount remainingTurns :: acc)
+        indexes =
+            List.range 1 totalTurnsCount
+
+        playersSequence =
+            List.concat (List.repeat totalRoundsCount players)
     in
-    List.reverse (buildRoundsInner turns [])
+    List.map3 Turn indexes turns playersSequence
 
 
 playerToBarColor : Player.Color -> String
@@ -71,43 +71,86 @@ playerToBarColor color =
             Chart.Attributes.brown
 
 
+viewBar : Turn -> Chart.Element Turn msg
+viewBar turn =
+    let
+        color =
+            playerToBarColor turn.player.color
+
+        position =
+            { x1 = toFloat turn.index - 0.4
+            , x2 = toFloat turn.index + 0.4
+            , y1 = 0
+            , y2 = toFloat turn.duration
+            }
+    in
+    Chart.custom
+        { name = turn.player.name
+        , color = color
+        , position = position
+        , format = always "ms"
+        , data = turn
+        , render =
+            \plane ->
+                Chart.Svg.bar plane
+                    [ Chart.Attributes.color color
+                    , Chart.Attributes.roundTop 1
+                    ]
+                    position
+        }
+
+
+viewChart : List Int -> List Player -> Html msg
+viewChart turns players =
+    let
+        xMax =
+            List.length turns
+
+        yMax =
+            Maybe.withDefault 0 (List.maximum turns)
+    in
+    Chart.chart
+        [ Chart.Attributes.width 400
+        , Chart.Attributes.height 300
+        , Chart.Attributes.margin
+            { top = 0
+            , bottom = 20
+            , left = 0
+            , right = 10
+            }
+
+        -- x-axis range
+        , Chart.Attributes.range
+            [ Chart.Attributes.lowest 0.5 Chart.Attributes.exactly
+            , Chart.Attributes.highest (toFloat xMax + 0.5) Chart.Attributes.orHigher
+            ]
+
+        -- y-axis domain
+        , Chart.Attributes.domain
+            [ Chart.Attributes.lowest 0 Chart.Attributes.exactly
+            , Chart.Attributes.highest (toFloat yMax) Chart.Attributes.orHigher
+            ]
+        ]
+        [ Chart.xAxis
+            [ Chart.Attributes.noArrow
+            ]
+        , Chart.xLabels
+            [ Chart.Attributes.withGrid
+            , Chart.Attributes.ints
+            ]
+        , Chart.yAxis
+            [ Chart.Attributes.noArrow
+            ]
+        , buildTurnsDurationData turns players
+            |> List.map viewBar
+            |> Chart.list
+        ]
+
+
 view : Model -> Html msg
 view { turns, players } =
-    let
-        totalPlayersCount =
-            List.length players
-    in
     Html.div
         [ Html.Attributes.class "w-[400px] p-2"
         ]
-        [ Chart.chart
-            [ Chart.Attributes.width 400
-            , Chart.Attributes.height 300
-            ]
-            [ Chart.xAxis
-                [ Chart.Attributes.noArrow
-                ]
-            , Chart.xLabels []
-            , Chart.yAxis
-                [ Chart.Attributes.noArrow
-                ]
-            , Chart.bars
-                [ Chart.Attributes.margin 0
-                , Chart.Attributes.spacing 0
-                ]
-                (List.indexedMap
-                    (\index player ->
-                        let
-                            playerIndex =
-                                modBy totalPlayersCount index
-                        in
-                        Chart.barMaybe
-                            (Maybe.map toFloat << getTurnDurationFor playerIndex)
-                            [ Chart.Attributes.color (playerToBarColor player.color)
-                            ]
-                    )
-                    players
-                )
-                (buildRounds turns players)
-            ]
+        [ Html.Lazy.lazy2 viewChart turns players
         ]
