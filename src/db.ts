b2ef026407cf7abd2c846d97c679d8aa0148eeed
Anton Ovechkin <owanturist@gmail.com>
2021-12-04 18:41:31 +0100

    Update DB schemas

--- src/db.ts
+++ src/db.ts
@@ -1,149 +1,212 @@
 import Dexie from 'dexie'
+import { differenceInMilliseconds } from 'date-fns'
+
+import { Color } from './domain'
+
+// P U B L I C   E N T I T I E S
+
+export abstract class Turn {
+  abstract id: number
+  abstract white_die: null | number
+  abstract red_die: null | number
+  abstract event_die: null | string
+  abstract is_paused: boolean
+  abstract duration_ms: number
+  abstract duration_since: Date
+}
+
+export abstract class Player {
+  abstract id: number
+  abstract name: string
+  abstract color: Color['id']
+}
+
+export abstract class Game {
+  abstract id: number
+  abstract start_time: Date
+  abstract end_time: null | Date
+  abstract players: ReadonlyArray<Player>
+  abstract turns: ReadonlyArray<Turn>
+}
+
+// D B   D E F I N I T I O N
 
 const cast_id = <T>(payload: T): T & { id: number } => {
   return payload as T & { id: number }
 }
 
-interface GameEntity {
-  id: number
-  players_count: number
-  turns_count: number
-  start_time: Date
-  end_time: null | Date
+abstract class GameEntity {
+  abstract id: number
+  abstract current_turn_id: number
+  abstract start_time: Date
+  abstract end_time: null | Date
 }
 
-interface PlayerEntity {
-  id: number
-  game_id: number
-  name: string
-  color: string
+abstract class PlayerEntity extends Player {
+  abstract game_id: number
+  abstract next_player_id: number
+
+  public static toPublic({
+    game_id,
+    next_player_id,
+    ...player
+  }: PlayerEntity): Player {
+    return player
+  }
 }
 
-interface TurnEntity {
-  id: number
-  game_id: number
-  player_id: number
-  white_die: null | number
-  red_die: null | number
-  event_die: null | string
-}
+abstract class TurnEntity extends Turn {
+  abstract game_id: number
+  abstract player_id: number
+  abstract prev_turn_id: null | number
 
-interface TurnStatusEntity {
-  id: number
-  turn_id: number
-  is_paused: boolean
-  start_time: Date
-  end_time: null | Date
+  public static toPublic({
+    game_id,
+    player_id,
+    prev_turn_id,
+    ...turn
+  }: TurnEntity): Turn {
+    return turn
+  }
 }
 
 const db = new (class DB extends Dexie {
   public games!: Dexie.Table<GameEntity, number>
   public players!: Dexie.Table<PlayerEntity, number>
   public turns!: Dexie.Table<TurnEntity, number>
-  public turn_statuses!: Dexie.Table<TurnStatusEntity, number>
 
   public constructor() {
     super('Catan')
 
     this.version(1).stores({
-      games: '++id',
-      players: '++id, game_id',
-      turns: '++id, game_id, player_id',
-      turn_statuses: '++id, turn_id'
+      games: '++id, current_turn_id',
+      players: '++id, game_id, next_player_id',
+      turns: '++id, game_id, player_id, prev_turn_id'
     })
   }
 })()
 
-export interface Turn {
-  id: number
-  white_die: number | null
-  red_die: number | null
-  event_die: string | null
-  is_paused: boolean
-  duration_ms: number
-  duration_since: Date
-}
-
-export interface Player {
-  id: number
-  name: string
-  color: string
-}
-
-export interface Game {
-  id: number
-  start_time: Date
-  end_time: null | Date
-  players: ReadonlyArray<Player>
-  turns: ReadonlyArray<Turn>
-}
-
 /**
- * Creates new not paused turn
+ * Creates players recursively until all players have been created
  */
-const start_new_turn = async ({
+const create_players = async (
+  game_id: number,
+  players: ReadonlyArray<PlayerPayload>
+): Promise<number> => {
+  const create_next_player = async (index: number): Promise<number> => {
+    const current_player = players[index]
+
+    // ends of recursion on the last player so return fake id and override it later
+    if (current_player == null) {
+      return Promise.resolve(-1)
+    }
+
+    return await db.players.add(
+      cast_id({
+        game_id,
+        ...current_player,
+        next_player_id: await create_next_player(index + 1)
+      })
+    )
+  }
+
+  const first_player_id = await create_next_player(0)
+  const last_player = await db.players.where('game_id').equals(game_id).last()
+
+  if (last_player == null) {
+    throw new Error(`No players created in game ${game_id}`)
+  }
+
+  await db.players.update(last_player.id, {
+    next_player_id: first_player_id
+  })
+
+  return first_player_id
+}
+
+const create_turn = async ({
   game_id,
-  player_id
+  player_id,
+  prev_turn_id = null,
+  start_time = new Date()
 }: {
   game_id: number
   player_id: number
+  prev_turn_id?: null | number
+  start_time?: Date
 }): Promise<number> => {
-  const game = await get_game_entity(game_id)
+  const next_turn_id = await db.turns.add(
+    cast_id({
+      game_id,
+      player_id,
+      prev_turn_id,
+      white_die: null,
+      red_die: null,
+      event_die: null,
+      is_paused: false,
+      duration_ms: 0,
+      duration_since: start_time
+    })
+  )
 
   await db.games.update(game_id, {
-    turns_count: game.turns_count + 1
+    current_turn_id: next_turn_id
   })
 
-  return db.turns
-    .add(
-      cast_id({
-        game_id,
-        player_id,
-        white_die: null,
-        red_die: null,
-        event_die: null
-      })
+  return next_turn_id
+}
+
+/**
+ * Creates next turn, finishes previous.
+ */
+export const next_turn = async (
+  game_id: number,
+  dice: {
+    white_die: number
+    red_die: number
+    event_die: string
+  }
+): Promise<number> => {
+  const game = await get_game_by_id(game_id)
+
+  // following turns
+  const current_turn = await db.turns.get(game.current_turn_id)
+
+  if (current_turn == null) {
+    throw new Error(
+      `Previous turn with id ${game.current_turn_id} not found in game ${game_id}`
     )
-    .then(turn_id => {
-      return db.turn_statuses.add(
-        cast_id({
-          turn_id,
-          is_paused: false,
-          start_time: game.turns_count === 0 ? game.start_time : new Date(),
-          end_time: null
-        })
-      )
-    })
+  }
+
+  const current_player = await db.players.get(current_turn.player_id)
+
+  if (current_player == null) {
+    throw new Error(
+      `Player with id ${current_turn.player_id} not found in game ${game_id}`
+    )
+  }
+
+  const now = new Date()
+  const current_turn_diff_duration = current_turn.is_paused
+    ? 0
+    : differenceInMilliseconds(now, current_turn.duration_since)
+
+  await db.turns.update(current_turn.id, {
+    ...dice,
+    is_paused: false,
+    duration_since: now,
+    duration_ms: current_turn.duration_ms + current_turn_diff_duration
+  })
+
+  return create_turn({
+    game_id,
+    player_id: current_player.next_player_id,
+    prev_turn_id: current_turn.id,
+    start_time: now
+  })
 }
 
-const get_turn_status = (
-  turn_id: number
-): Promise<Pick<Turn, 'is_paused' | 'duration_ms' | 'duration_since'>> => {
-  return db.turn_statuses
-    .where('turn_id')
-    .equals(turn_id)
-    .toArray()
-    .then(turn_statuses => {
-      let is_paused = false
-      let duration_ms = 0
-      // it always gets re-defined
-      let duration_since = new Date()
-
-      for (const status of turn_statuses) {
-        if (status.end_time == null) {
-          // only the last turn status is has no end time
-          is_paused = status.is_paused
-          duration_since = status.start_time
-        } else {
-          duration_ms += status.end_time.getTime() - status.start_time.getTime()
-        }
-      }
-
-      return { is_paused, duration_ms, duration_since }
-    })
-}
-
-const get_game_entity = (game_id: number): Promise<GameEntity> => {
+const get_game_by_id = (game_id: number): Promise<GameEntity> => {
   return db.games.get(game_id).then(game => {
     if (game == null) {
       return Promise.reject(new Error(`Game with id ${game_id} not found`))
@@ -155,33 +218,19 @@ const get_game_entity = (game_id: number): Promise<GameEntity> => {
 
 export const get_game = (game_id: number): Promise<null | Game> => {
   return Promise.all([
-    get_game_entity(game_id).then(
-      ({ players_count, turns_count, ...game }) => game
-    ),
-
-    db.players
-      .where('game_id')
-      .equals(game_id)
-      .toArray()
-      .then(players => players.map(({ game_id: _, ...player }) => player)),
-
-    db.turns
-      .where('game_id')
-      .equals(game_id)
-      .toArray()
-      .then(turns => {
-        return Promise.all(
-          turns.map(({ game_id: _, player_id, ...turn }) =>
-            get_turn_status(turn.id).then(status => ({ ...turn, ...status }))
-          )
-        )
-      })
-  ]).then(([game, players, turns]) => ({ ...game, players, turns }))
+    get_game_by_id(game_id),
+    db.players.where('game_id').equals(game_id).toArray(),
+    db.turns.where('game_id').equals(game_id).toArray()
+  ]).then(([{ current_turn_id, ...game }, players, turns]) => ({
+    ...game,
+    players: players.map(PlayerEntity.toPublic),
+    turns: turns.map(TurnEntity.toPublic)
+  }))
 }
 
 export interface PlayerPayload {
   name: string
-  color: string
+  color: Color['id']
 }
 
 export const start_game = async (
@@ -193,28 +242,24 @@ export const start_game = async (
     )
   }
 
-  const start_time = new Date()
+  const now = new Date()
   const game_id = await db.games.add(
     cast_id({
-      players_count: players.length,
-      turns_count: 0,
-      start_time,
+      // fake id will be replaced by first turn
+      current_turn_id: -1,
+      start_time: now,
       end_time: null
     })
   )
 
-  const [first_player, ...rest_players] = players.map(player =>
-    db.players.add(cast_id({ game_id, ...player }))
-  )
+  const first_player_id = await create_players(game_id, players)
 
-  await Promise.all([
-    start_new_turn({
-      game_id,
-      player_id: await first_player!
-    }),
-
-    Promise.all(rest_players)
-  ])
+  // create first turn
+  await create_turn({
+    game_id,
+    player_id: first_player_id,
+    start_time: now
+  })
 
   return game_id
 }
--- src/domain.ts
+++ src/domain.ts
@@ -1,34 +1,34 @@
-export abstract class Color<ID extends string = string> {
-  abstract readonly id: ID
+export abstract class Color {
+  abstract readonly id: 'red' | 'blue' | 'yellow' | 'white' | 'brown' | 'green'
   abstract readonly label: string
   abstract readonly hex: string
 
-  public static red: Color<'red'> = {
+  public static red: Color = {
     id: 'red',
     label: 'Red',
     hex: '#ff595e'
   }
-  public static blue: Color<'blue'> = {
+  public static blue: Color = {
     id: 'blue',
     label: 'Blue',
     hex: '#1982c4'
   }
-  public static yellow: Color<'yellow'> = {
+  public static yellow: Color = {
     id: 'yellow',
     label: 'Yellow',
     hex: '#ffca3a'
   }
-  public static white: Color<'white'> = {
+  public static white: Color = {
     id: 'white',
     label: 'White',
     hex: '#e2e2df'
   }
-  public static brown: Color<'brown'> = {
+  public static brown: Color = {
     id: 'brown',
     label: 'Brown',
     hex: '#d68c45'
   }
-  public static green: Color<'green'> = {
+  public static green: Color = {
     id: 'green',
     label: 'Green',
     hex: '#8ac926'
