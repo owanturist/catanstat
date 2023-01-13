7ba9c902ed5546c3ecb9f44edc785218e9368ee4
Anton Ovechkin <owanturist@gmail.com>
2023-01-13 21:46:39 +0100

    Don't use strict mode due to beautiful dnd

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,10 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { App } from './App'
 
+// React.StrictMode is gone due to beautiful-dnd incompatibility
+// https://github.com/atlassian/react-beautiful-dnd/issues/2407#issuecomment-1137221247
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
